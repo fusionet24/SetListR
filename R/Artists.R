c8ccc995593a8ccfbe0e37c6c166d2375acb514c
Scott James Bell <webmaster@fusionet24.com>
2018-01-06 00:15:19 +0000

    Add files via upload

---
+++ R/Artists.R
@@ -0,0 +1,27 @@
+#' Get Artists Set Lists
+#'
+#' @param API_KEY Your Setlist FM API Key
+#' @param mbid The Artists Musicbrainz MBID
+#' @param page The page of the site you wish to gather data from
+#' @param Addtional_Headers Any addtional headers accepted by the api. Such as Language
+#' @param ResponseType Whether you want JSON or XML responses (currently it is just a preprocessed list)
+#'
+#' @return a list object of the artists set lists for a given page
+#'
+#' @examples
+#' getArtistSetLists(API_KEY = "KEY",mbid = 'aca5718f-4f49-4439-b8f6-209db3f11757')
+#' SetListR::getArtistSetLists(API_KEY = "KEY",mbid = '0bfba3d3-6a04-4779-bb0a-df07df5b0558',page = 12, ResponseType = 'json')
+getArtistSetLists <- function(API_KEY,mbid,page = 1, Addtional_Headers = NULL, ResponseType= NULL){
+
+  Request_String <- paste0(mbid,"/setlists?p=",page)
+  if (is.null(ResponseType)) {
+
+    res <- BuildRequest(APIKEY = API_KEY, Endpoint = "artist",Query_String = Request_String)
+  }
+  else
+  {
+    res <- BuildRequest(APIKEY = API_KEY, Endpoint = "artist",Query_String = Request_String,Content_Type = ResponseType)
+
+  }
+  res
+}
